18e1f9fac8d1801b3b9f1aabd3431ec19178dd23
SKarakayaa <sefakarakayaa@gmail.com>
2020-12-13 19:30:50 +0300

    e-commerce created

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import "./homepage.styles.scss";
+
+import Directory from "../../components/directory/directory.component";
+import React from "react";
+const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+export default HomePage;
