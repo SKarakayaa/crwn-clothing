df2e9f095a204e923c7cdf9e84dc11b0b22b640b
SKarakayaa <sefakarakayaa@gmail.com>
2021-01-03 14:19:29 +0300

    collection-overview and collection page made as container

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,11 +3,12 @@ import { applyMiddleware, createStore } from "redux";
 import logger from "redux-logger";
 import { persistStore } from "redux-persist";
 import rootReducer from "./root-reducer";
+import thunk from "redux-thunk";
 
-const middlewares = [];
+const middlewares = [thunk];
 
-if(process.env.NODE_ENV === 'development'){
-    middlewares.push(logger);
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
